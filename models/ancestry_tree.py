6138cea3510a80437ff81d9be98e5999ee669453
btf9895 <btf9895@rit.edu>
2023-07-31 14:59:58 -0400

    Implement all relational functionality between family members\

--- models/ancestry_tree.py
+++ models/ancestry_tree.py
@@ -48,9 +48,9 @@ class AncestryTreeMember(models.Model):
     #family info
     mother = fields.Many2one(string="Mother", comodel_name="ancestry.tree.member")
     father = fields.Many2one(string="Father", comodel_name="ancestry.tree.member")
-    siblings = fields.Many2many("ancestry.tree.member", "siblings", "sibling_one", "sibling_two", string="Siblings")
-    spouses = fields.Many2many("ancestry.tree.member", "spouses", 'spouse_one', 'spouse_two', string="Spouses")
-    children = fields.One2many(string="Children", comodel_name="ancestry.tree.member", inverse_name="mother", compute="_update_children")
+    children = fields.Many2many(string="Children", comodel_name="ancestry.tree.member", relation="children", column1="parent", column2="child")
+    siblings = fields.Many2many(string="Siblings", comodel_name="ancestry.tree.member", relation="siblings", column1="member", column2="sibling")
+    spouses = fields.Many2many(string="Spouses", comodel_name="ancestry.tree.member", relation="spouses", column1='member', column2='spouse')
 
     #additional info
     events = fields.One2many(string="Life Events", comodel_name="ancestry.event", inverse_name="tree_member_id")
@@ -61,12 +61,80 @@ class AncestryTreeMember(models.Model):
     tree_id = fields.Many2one(string="Related Tree", comodel_name="ancestry.tree")
     status = fields.Selection([("unconfirmed","Unconfirmed"),("confirmed","Confirmed")], string="Status", default="unconfirmed")
     # is_wizard_view = fields.Boolean(string="Is Wizard View Entry")
+    # TESTMANY2MANY = fields.Many2many(string="TESTING PURPOSES ONLY", comodel_name="ancestry.tree.member", relation="test_many2many", column1="test_many2many_column1", column2 = "test_many2many_column2")
 
-    @api.depends("mother")
-    def _update_children(self):
-        self.children = self.children
-        self.mother.children.write(self)
-        print(self)
+    #----------------------------------------------------------#
+    # TODO:
+    # 1. On change of mother or father, remove self from the previous mother or father's children
+    # 2. Add function for on change of children, to ensure correct two way functionality (do we want to implement this?)
+
+    @api.onchange("mother")
+    def _onchange_mother(self):
+        # remove child from previous mother's record
+        if self._origin.mother:                         #TODO: why isnt this working?
+            self._origin.mother.children -= self        #   <--------------------
+        # write child to new mother's record, union operator handles duplicate records
+        self.mother.children |= self
+
+    @api.onchange("father")
+    def _onchange_father(self):
+        # remove child from previous father's record
+        if self._origin.father:                         #TODO: why isnt this working?
+            self._origin.father.children -= self        #   <--------------------
+        # write child to new father's record, union operator handles duplicate records
+        self.father.children |= self
+
+    @api.onchange("children")
+    def _onchange_children(self):
+        print('do we need to implement this functionality?')
+
+    
+    def write(self, vals):
+        """
+        Update many2manys, serves as:
+            @api.onchange("siblings")
+            @api.onchange("spouses")
+        """
+        for member in self:
+            # update siblings
+            if 'siblings' in vals:
+                new_siblings = vals['siblings'][0][2] if vals.get('siblings') else []
+                existing_siblings = member.siblings.ids
+                # add current member to the siblings list of new siblings
+                if new_siblings:
+                    for new_sibling_id in new_siblings:
+                        if new_sibling_id not in existing_siblings:
+                            self.env['ancestry.tree.member'].browse(new_sibling_id).write({
+                                'siblings': [(4, member.id, 0)]
+                            })
+                # remove current member from the siblings list of old siblings
+                if existing_siblings:
+                    for sibling_id in existing_siblings:
+                        if new_siblings and sibling_id not in new_siblings:
+                            self.env['ancestry.tree.member'].browse(sibling_id).write({
+                                'siblings': [(3, member.id, 0)]
+                            })
+            # update spouses
+            if 'spouses' in vals:
+                new_spouses = vals['spouses'][0][2] if vals.get('spouses') else []
+                existing_spouses = member.spouses.ids
+                # add current member to the spouses list of new spouses
+                if new_spouses:
+                    for new_spouse_id in new_spouses:
+                        if new_spouse_id not in existing_spouses:
+                            self.env['ancestry.tree.member'].browse(new_spouse_id).write({
+                                'spouses': [(4, member.id, 0)]
+                            })
+                # remove current member from the spouses list of old spouses
+                if existing_spouses:
+                    for spouse_id in existing_spouses:
+                        if new_spouses and spouse_id not in new_spouses:
+                            self.env['ancestry.tree.member'].browse(spouse_id).write({
+                                'spouses': [(3, member.id, 0)]
+                            })
+        return super(AncestryTreeMember, self).write(vals)
+
+    #----------------------------------------------------------#
 
     def confirm_ancestry_tree_member(self):
         self.ensure_one()
@@ -79,8 +147,8 @@ class AncestryTreeMember(models.Model):
         self.status = 'confirmed'
 
     def show_related_tree(self):
-         # print('uh oh')
-         # debug
+        # print('uh oh')
+        # debug
         #  c = self.env.context
         # tree = self.env.context.get('active_id')
         # t = 
